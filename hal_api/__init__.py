e53bcfc70051a6d3136676be06edf67a23aa1358
yutong-fei <yutong.fei@ccsd.cnrs.fr>
2026-07-29 08:38:09 +0200

    ajouter un fichier

---
+++ hal_api/__init__.py
@@ -0,0 +1,4 @@
+from hal_api.hal_api_get_author_affiliations import hal_api_get_author_affiliations
+from hal_api.search_authors import search_authors
+
+__all__ = ["hal_api_get_author_affiliations", "search_authors"]
